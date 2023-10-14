08fdb92df6aae8bd08eddd8d27c3f82596eaad95
李磊 <lilei@dongyou.com>
2023-10-14 22:32:12 +0800

    Update regex_test.py

--- tests/regex_test.py
+++ tests/regex_test.py
@@ -1,5 +1,6 @@
 import re
 import unittest
+from app.core.version import VersionParser
 
 
 class RegexpTestCase(unittest.TestCase):
@@ -11,6 +12,13 @@ class RegexpTestCase(unittest.TestCase):
 
         self.assertIsInstance(m, re.Match)
         self.assertEqual(m.group('suffix'), 'w')
+
+    def test_version_latest(self):
+        ps = VersionParser(pattern=r'^(?P<major>\d+)\.(?P<minor>\d+)\.(?P<patch>\d+)(?P<suffix>[a-z])?$')
+
+        v1 = ['1.1.1b', '1.1.1a', '1.1.1c', '1.1.1w', '1.1.1v', '1.1.2t', '1.2.0', '1.12.0']
+
+        self.assertEqual(ps.latest(v1), '1.12.0')
 
 
 if __name__ == '__main__':
