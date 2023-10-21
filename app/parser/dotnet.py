1c682fd211531db7f06e97c865ba85b2af3c51fb
李磊 <lilei@dongyou.com>
2023-10-21 17:40:16 +0800

    Update dotnet.py

--- app/parser/dotnet.py
+++ app/parser/dotnet.py
@@ -1,6 +1,32 @@
+from __future__ import annotations
+
+import json
+from typing import List, Optional
+
+from pydantic import BaseModel, Field
+
 from app.core.config import AppSettingSoftItem
 from app.core.version import VersionHelper
 from . import Assistant
+
+
+class ReleasesIndexItem(BaseModel):
+    channel_version: str = Field(..., alias='channel-version')
+    latest_release: str = Field(..., alias='latest-release')
+    latest_release_date: str = Field(..., alias='latest-release-date')
+    security: bool
+    latest_runtime: str = Field(..., alias='latest-runtime')
+    latest_sdk: str = Field(..., alias='latest-sdk')
+    product: str
+    release_type: str = Field(..., alias='release-type')
+    support_phase: str = Field(..., alias='support-phase')
+    eol_date: Optional[str] = Field(..., alias='eol-date')
+    releases_json: str = Field(..., alias='releases.json')
+
+
+class Model(BaseModel):
+    schema: str = Field(..., alias='$schema')
+    releases_index: List[ReleasesIndexItem] = Field(..., alias='releases-index')
 
 
 class Parser:
@@ -11,12 +37,11 @@ class Parser:
                              download_urls=item.download_urls)
 
         # Make an HTTP request.
-        url, http_status_code, _, data_r = await assist.get('https://github.com/dotnet/core/raw/main/release-notes/releases-index.json',
-                                                            is_json=True)
+        url, http_status_code, _, data_s = await assist.get('https://raw.githubusercontent.com/dotnet/core/main/release-notes/releases-index.json')
 
-        if isinstance(data_r, dict) and 'prefixes' in data_r:
-            for v in data_r['prefixes']:
-                vhlp.add(str(v).removeprefix('channels/stable/release/').rstrip('/'))
+        data_r = json.loads(data_s)
+        dotnet_release = Model.model_validate(data_r)
+        print(dotnet_release)
 
         # Perform actions such as sorting.
         vhlp.done()
