78cfe7e5c01d90909d84e5b9b06f09e15a4f9f58
李磊 <lilei@dongyou.com>
2023-10-21 21:26:11 +0800

    Add chrome

---
+++ app/parser/chrome.py
@@ -0,0 +1,30 @@
+from app.core.config import AppSettingSoftItem
+from app.core.version import VersionHelper
+from . import Assistant
+
+
+class Parser:
+    @staticmethod
+    async def parse(assist: Assistant, item: AppSettingSoftItem):
+        # Create VersionHelper instance.
+        vhlp = VersionHelper(name=item.name, pattern=r'^(?P<version>(?P<major>\d+)\.(?P<minor>\d+)\.(?P<patch>\d+)\.(?P<fix>\d+))$',
+                             download_urls=item.download_urls)
+
+        # Make an HTTP request.
+        url, http_status_code, _, data_r = await assist.get(
+            'https://versionhistory.googleapis.com/v1/chrome/platforms/win/channels/stable/versions/all/releases',
+            is_json=True)
+
+        if isinstance(data_r, dict) and 'releases' in data_r:
+            for v in data_r['releases']:
+                vhlp.add(v['version'])
+
+        # Perform actions such as sorting.
+        vhlp.done()
+
+        # Output JSON file.
+        await assist.create(name=item.name,
+                            url=item.url if item.url else 'https://www.google.com/chrome/',
+                            version=vhlp.latest,
+                            all_versions=vhlp.versions,
+                            download_links=vhlp.download_links)
