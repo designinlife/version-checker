ea86e4124e5b883ee50c27716f02e862fda2a8da
李磊 <lilei@dongyou.com>
2023-10-21 14:53:03 +0800

    Add dart

---
+++ app/parser/dart.py
@@ -0,0 +1,30 @@
+from app.core.config import AppSettingSoftItem
+from app.core.version import VersionHelper
+from . import Assistant
+
+
+class Parser:
+    @staticmethod
+    async def parse(assist: Assistant, item: AppSettingSoftItem):
+        # Create VersionHelper instance.
+        vhlp = VersionHelper(name=item.name, pattern=r'^(?P<version>(?P<major>\d+)\.(?P<minor>\d+)\.(?P<patch>\d+))$',
+                             download_urls=item.download_urls)
+
+        # Make an HTTP request.
+        url, http_status_code, _, data_r = await assist.get('https://storage.googleapis.com/storage/v1/b/dart-archive/o',
+                                                            params={'delimiter': '/', 'prefix': 'channels/stable/release/', 'alt': 'json'},
+                                                            is_json=True)
+
+        if isinstance(data_r, dict) and 'prefixes' in data_r:
+            for v in data_r['prefixes']:
+                vhlp.add(str(v).removeprefix('channels/stable/release/').rstrip('/'))
+
+        # Perform actions such as sorting.
+        vhlp.done()
+
+        # Output JSON file.
+        await assist.create(name=item.name,
+                            url=item.url if item.url else 'https://dart.dev/',
+                            version=vhlp.latest,
+                            all_versions=vhlp.versions,
+                            download_links=vhlp.download_links)
