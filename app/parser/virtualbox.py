61c527144ce05488d4fac8dac5e958d837b8ed86
Lei.Lee <codeplus@qq.com>
2024-09-19 12:28:09 +0800

    Update virtualbox.py

--- app/parser/virtualbox.py
+++ app/parser/virtualbox.py
@@ -1,5 +1,6 @@
 import re
 from asyncio import Semaphore
+from urllib.parse import urljoin
 
 from bs4 import BeautifulSoup
 from loguru import logger
@@ -17,42 +18,57 @@ class Parser(Base):
 
         async with sem:
             # Make an HTTP request.
-            _, status, _, data_s = await self.request('GET', 'https://www.virtualbox.org/wiki/Downloads',
+            _, status, _, data_s = await self.request('GET', 'https://download.virtualbox.org/virtualbox/',
                                                       is_json=False)
 
             soup = BeautifulSoup(data_s, 'html5lib')
 
-            latest_element = soup.select_one('#wikipage > h3:nth-of-type(1)')
-            download_link_elements = soup.select('#wikipage > ul:nth-of-type(1) > li > a[class=ext-link]')
-            extension_pack_link_elements = soup.select('#wikipage > ul:nth-of-type(3) > li > a[class=ext-link]')
-            sdk_link_elements = soup.select('#wikipage > ul:nth-of-type(4) > li > a[class=ext-link]')
+            link_elements = soup.select('pre > a[href]')
 
             exp_ver = re.compile(
-                r'^VirtualBox (?P<version>(?P<major>\d+)\.(?P<minor>\d+)\.(?P<patch>\d+)) platform packages$',
+                r'^(?P<version>(?P<major>\d+)\.(?P<minor>\d+)\.(?P<patch>\d+))$',
                 flags=re.I)
 
-            m = exp_ver.match(latest_element.text.strip())
+            for v in link_elements:
+                m = exp_ver.match(v.attrs['href'].rstrip('/'))
 
-            if m:
-                latest_version = m.group('version')
-                vhlp.append(latest_version)
+                if m:
+                    version = m.group('version')
+                    vhlp.append(version)
 
-                # Installer
-                if download_link_elements:
-                    for v in download_link_elements:
-                        if v.attrs['href'].endswith('Win.exe'):
-                            vhlp.add_download_url(v.attrs['href'])
+                    # Installer
+                    # if download_link_elements:
+                    #     for v in download_link_elements:
+                    #         if v.attrs['href'].endswith('Win.exe'):
+                    #             vhlp.add_download_url(v.attrs['href'])
 
-                # Extension Pack
-                if extension_pack_link_elements:
-                    for v in extension_pack_link_elements:
-                        if v.attrs['href'].endswith('.vbox-extpack'):
-                            vhlp.add_download_url(v.attrs['href'])
+                    # Extension Pack
+                    # if extension_pack_link_elements:
+                    #     for v in extension_pack_link_elements:
+                    #         if v.attrs['href'].endswith('.vbox-extpack'):
+                    #             vhlp.add_download_url(v.attrs['href'])
 
-                # SDK
-                if sdk_link_elements:
-                    for v in sdk_link_elements:
-                        vhlp.add_download_url(v.attrs['href'])
+                    # SDK
+                    # if sdk_link_elements:
+                    #     for v in sdk_link_elements:
+                    #         vhlp.add_download_url(v.attrs['href'])
+
+            latest_version = vhlp.latest_version
+            print(latest_version)
+
+            url, status, _, data_s = await self.request('GET', f'https://download.virtualbox.org/virtualbox/{latest_version.version}/',
+                                                        is_json=False)
+
+            soup = BeautifulSoup(data_s, 'html5lib')
+
+            link_elements = soup.select('pre > a[href]')
+
+            for v in link_elements:
+                href = v.attrs['href']
+
+                if isinstance(href, str):
+                    if href.endswith('.exe') or href.endswith('.zip') or href.endswith('.vbox-extpack'):
+                        vhlp.add_download_url(urljoin(url.__str__(), href))
 
             logger.debug(f'Name: {soft.name}, Versions: {vhlp.versions}, Summary: {vhlp.summary}')
 
