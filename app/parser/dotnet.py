682be69cc4140c30877c8ddbdb126e9834a79487
Lei.Lee <codeplus@qq.com>
2025-03-12 16:46:20 +0800

    Update dotnet.py

--- app/parser/dotnet.py
+++ app/parser/dotnet.py
@@ -49,8 +49,8 @@ class Parser(Base):
         tasks = []
 
         for v in dotnet_release.releases_index:
-            if v.release_type in ('lts', 'sts') and (v.eol_date is None or (v.eol_date is not None and arrow.get(v.eol_date,
-                                                                                                           'YYYY-MM-DD') > arrow.now())):
+            if v.release_type in ('lts', 'sts') and v.support_phase == 'active' and (v.eol_date is None or (v.eol_date is not None and arrow.get(v.eol_date,
+                                                                                                                                                 'YYYY-MM-DD') > arrow.now())):
                 tasks.append(self.request('GET', v.releases_json, is_json=True))
 
         results = await asyncio.gather(*tasks, return_exceptions=True)
