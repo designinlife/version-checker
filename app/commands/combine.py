083d409c2a51d46f28a55a42cea0c23c4fc426c2
Lei.Lee <codeplus@qq.com>
2025-03-15 14:22:48 +0800

    Update combine.py

--- app/commands/combine.py
+++ app/commands/combine.py
@@ -59,12 +59,13 @@ def compare_json_data(new_data: List[Dict], old_data: Optional[List[Dict]] = Non
 
     for new_item in new_data:
         if 'name' in new_item and 'latest' in new_item:
-            name = new_item['display_name'] if 'display_name' in new_item and new_item['display_name'] != '' else new_item['name']
+            name = new_item['name']
+            display_name = new_item['display_name'] if 'display_name' in new_item and new_item['display_name'] != '' else new_item['name']
             latest = new_item['latest']
             previous = old_data_dict.get(name)
 
             if previous != latest:
-                updates.append((name, latest, previous if previous is not None else ""))
+                updates.append((display_name, latest, previous if previous is not None else ""))
     return updates
 
 
